e9dca4800055fc779557f396c6852b8bc93d254e
Kate Sanders <SandersKM@Hendrix.edu>
2019-02-20 12:16:11 -0600

    findIDs method to declutter OnCreate

--- app/src/main/java/com/example/rockpaperscissors/GameActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/GameActivity.java
@@ -2,12 +2,22 @@ package com.example.rockpaperscissors;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class GameActivity extends AppCompatActivity {
+
+    Button rock, paper, scissors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        findIDs();
+    }
+    
+    public void findIDs(){
         setContentView(R.layout.activity_game);
+        rock = findViewById(R.id.stone_button);
+        paper = findViewById(R.id.paper_button);
+        scissors = findViewById(R.id.scissors_button);
     }
 }
--- app/src/main/java/com/example/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -5,16 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button invite;
+    TextView other_IP, other_port;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button invite = findViewById(R.id.request_button);
+        findIDs();
         invite.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -22,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(forwardIntent);
             }
         });
+    }
+
+    public void findIDs(){
+        setContentView(R.layout.activity_main);
+        invite = findViewById(R.id.request_button);
+        other_IP = findViewById(R.id.IP_textbox);
+        other_port = findViewById(R.id.Port_textbox);
     }
 
     // TODO overall stats and stats by player
