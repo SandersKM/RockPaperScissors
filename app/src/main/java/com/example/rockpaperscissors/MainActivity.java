de5d412aa48ab666331123b60bb2f81dc23e0111
Kate Sanders <SandersKM@Hendrix.edu>
2019-02-19 21:42:48 -0600

    Added and linked second screen

--- app/src/main/java/com/example/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -1,13 +1,29 @@
 package com.example.rockpaperscissors;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button invite;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button invite = findViewById(R.id.request_button);
+        invite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent forwardIntent = new Intent(MainActivity.this, Game.class);
+                startActivity(forwardIntent);
+            }
+        });
     }
+
+
 }
