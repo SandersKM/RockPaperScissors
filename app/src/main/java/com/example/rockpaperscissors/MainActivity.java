364fd40047a90ce64a30317c2726f6d769749747
Kate Sanders <SandersKM@Hendrix.edu>
2019-02-20 12:42:47 -0600

    Shows devices IP address at top of MainActivity

--- app/src/main/java/com/example/rockpaperscissors/MainActivity.java
+++ app/src/main/java/com/example/rockpaperscissors/MainActivity.java
@@ -4,18 +4,24 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.net.SocketException;
+import java.net.Socket;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     Button invite;
-    TextView other_IP, other_port;
+    TextView my_IP, other_IP, other_port;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         findIDs();
+        setMy_IP();
         inviteListener();
     }
 
@@ -32,8 +38,17 @@ public class MainActivity extends AppCompatActivity {
     public void findIDs(){
         setContentView(R.layout.activity_main);
         invite = findViewById(R.id.request_button);
+        my_IP = findViewById(R.id.IP_device);
         other_IP = findViewById(R.id.IP_textbox);
         other_port = findViewById(R.id.Port_textbox);
+    }
+
+    public void setMy_IP(){
+        try {
+            my_IP.setText(Utilities.getLocalIpAddress());
+        } catch (SocketException e) {
+            Log.e("MainActivity", "Threw exception when finding ip address");
+        }
     }
 
     // TODO overall stats and stats by player
