9a524617299513832a02029d5f5d0bb8d0165785
Danny-Turner <46805389+Danny-Turner@users.noreply.github.com>
2019-02-28 22:54:16 -0600

    Commented out move listener
    partially implemented move listener was crashing program. I commented it out

---
+++ app/src/main/java/com/example/rockpaperscissors/Game.java
@@ -0,0 +1,24 @@
+package com.example.rockpaperscissors;
+
+/*public class Game {
+    static Move myMove, otherMove;
+    static boolean moveSent = false;
+    static boolean moveReceived = false;
+    static boolean turnCompleted = false;
+
+    public static Move getMyMove() {return myMove;}
+    public static void setMyMove (Move m) {myMove = m;}
+
+    public static Move getOtherMove() {return otherMove;}
+    public static void setOtherMove (Move m) {myMove = m; }
+
+    public static void setMoveSent(boolean value) {moveSent = value; }
+    public static boolean getMoveSent() {return moveSent; }
+
+    public static void setMoveReceived(boolean value) {moveReceived = value; }
+    public static boolean getMoveReceived() {return moveReceived; }
+
+    public static void setturnCompleted(boolean value) {turnCompleted = value; }
+    public static boolean getTurnCompleted() {return turnCompleted; }
+}
+*/
